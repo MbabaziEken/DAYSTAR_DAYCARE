f8086a330a2b53976ab129d9df31faf80c3b55d2
MbabaziEken <mbabazieken@gmail.com>
2024-04-24 22:06:20 +0300

    ALL

---
+++ main.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+const path = require ("path");
+
+const publicdir = path-join(__dirname, "./public");
+console.log(__dirname);
+console.log(publicdir);
+
+app.use(express.static(publicdir));
+
+app.set("view engine","");
+
+
+// Routes
+app.get("/",(req,res) => {
+    res.render("daystardollstall")
+});
+
+app.listen(4000,() => {
+    console.log ("server is running on port 4000")
+});

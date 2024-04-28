4f7ea11fa206d3ba6ec71a6ae7b36cbf3c0dfa6a
MbabaziEken <mbabazieken@gmail.com>
2024-04-28 18:45:44 +0300

    changes

---
+++ public/js/signup.js
@@ -0,0 +1,12 @@
+const form = document.getElementById("form");
+const FullName = document.querrySelector(".")
+
+form.addEventListener("submit", (e) => {
+e.preventDefault ();
+validInput ();
+});
+
+//.....step.......//
+const validInput = () => {
+
+}

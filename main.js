ee3fc75500b2c4df5205660dcc1bc1bca778263a
MbabaziEken <mbabazieken@gmail.com>
2024-04-29 03:18:52 +0300

    stage

--- main.js
+++ main.js
@@ -1,20 +1,32 @@
-
 const express = require("express");
 const app = express();
 const path = require ("path");
+const mongoose = require("mongoose");
+// rendering engine
+const pug = require("pug");
+// giving functionality for storing user sessions
+const expressSession = require("express-session");
 
-const publicdir = path-join(__dirname, "./public");
-console.log(__dirname);
-console.log(publicdir);
+require("dotenv").config()
 
+// mongoose configurations
+mongoose.connect(process.env.DATABASE,{})
+.then(() => {
+    console.log('connect to DATABASE')
+})
+.catch(() => {
+    console.log('error connecting to DATABASE')
+})
+
+const publicdir = path.join(__dirname, "./public");
 app.use(express.static(publicdir));
 
-app.set("view engine","");
+app.set("view engine","pug");
 
 
 // Routes
 app.get("/",(req,res) => {
-    res.render("")
+    res.render("./views/splash")
 });
 
 app.listen(4000,() => {
--- public/js/signup.js
+++ public/js/signup.js
@@ -2,18 +2,58 @@
 const form = documentget.
 ElementById("form");
 
+// Fields
 const fullName = documentget.ElementById("fullName");
 const EBG = documentget.ElementById("EBG");
 const WE = documentget.ElementById("WE");
 const Gender = documentget.ElementById("Gender");
 const Email = documentget.ElementById("Email");
 const Password = documentget.ElementById("Password");
+const comfirmPassword = documentget.ElementById("comfirmPassword");
 
 // validation
 function validatefullName(){
    // check if empty
    if(checkifempty(fullName)) return;
-
+// it has only letters
+   if(!checkIfOnlyLetters(field)) return;
+   return true;
 }
 
-function checkIfEmpty
+function checkIfempty(field){
+    if(isEmpty(field.value.trim())){
+    // set field invalid
+setinvalid(field, `${field.name} must not be empty`);
+    return true;
+    } else {
+        // set field valid
+        setValid(field);
+        return false;
+    }
+}
+function isEmpty(value){
+    if(value === '') return true;
+    return false;
+}
+
+function setInvalid(field,message){
+    field.className = `Invalid`
+    field.nextElementSibling.innerHTML = message;
+    field.nextElementSibling.style.color = red;
+}
+
+function setValid(field,message){
+    field.className = `Valid`
+    field.nextElementSibling.innerHTML = '';
+    field.nextElementSibling.style.color = green;
+}
+
+function checkIfonlyLetters(field){
+    if(/^[a-zA_Z ]+$/-TextDecoderStream(field.value)){
+        setValid(field)
+        return true;
+    } else{
+        setInvalid(field, `$ must contain only letters`)
+        return false;
+    }
+}
